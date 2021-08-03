a074a7413af050ae0eeb167bb12655736af4722b
taufiqjack <taufiqdwic@gmail.com>
2021-08-03 15:39:19 +0700

    adding tabbar

--- lib/pages/detailsmatch.dart
+++ lib/pages/detailsmatch.dart
@@ -10,7 +10,22 @@ class DetailsMatch extends StatefulWidget {
   _DetailsMatchState createState() => _DetailsMatchState();
 }
 
-class _DetailsMatchState extends State<DetailsMatch> {
+class _DetailsMatchState extends State<DetailsMatch>
+    with TickerProviderStateMixin {
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    tabController!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,106 +54,253 @@ class _DetailsMatchState extends State<DetailsMatch> {
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-        child: Card(
-          child: Container(
-            height: 213,
-            width: 366,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Warna.orange,
-                            borderRadius: BorderRadius.circular(5)),
-                        height: 20,
-                        width: 50,
-                        child: Center(
-                          child: Text(
-                            'LIVE',
-                            style: TextStyle(color: Colors.white),
+        child: Column(children: [
+          Card(
+            child: Container(
+              height: 213,
+              width: 366,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Warna.orange,
+                              borderRadius: BorderRadius.circular(5)),
+                          height: 20,
+                          width: 50,
+                          child: Center(
+                            child: Text(
+                              'LIVE',
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
-                    Image.asset(
-                      'assets/images/premierleague.png',
-                    ),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
-                    Text(
-                      'English Premier League',
-                      style: GoogleFonts.montserrat(),
-                    ),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
-                    Icon(
-                      Icons.notifications_rounded,
-                      color: Colors.purple,
-                    ),
-                  ],
-                ),
-                Padding(padding: EdgeInsets.all(5)),
-                Divider(
-                  height: 0.5,
-                ),
-                Padding(padding: EdgeInsets.all(10)),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30),
-                      child: Image.asset(
-                        'assets/images/leicaster.png',
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+                      Image.asset(
+                        'assets/images/premierleague.png',
                       ),
-                    ),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 15)),
-                    Column(
-                      children: [
-                        Text('17.06.2021',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 12, color: Colors.grey)),
-                        Text(
-                          '2 : 0',
-                          style: GoogleFonts.montserrat(
-                              fontWeight: FontWeight.bold, fontSize: 30),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                      Text(
+                        'English Premier League',
+                        style: GoogleFonts.montserrat(),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+                      Icon(
+                        Icons.notifications_rounded,
+                        color: Colors.purple,
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Divider(
+                    height: 0.5,
+                  ),
+                  Padding(padding: EdgeInsets.all(10)),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 30),
+                        child: Image.asset(
+                          'assets/images/leicaster.png',
                         ),
-                        Text('Fulltime',
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 15)),
+                      Column(
+                        children: [
+                          Text('17.06.2021',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 12, color: Colors.grey)),
+                          Text(
+                            '2 : 0',
                             style: GoogleFonts.montserrat(
-                                fontSize: 12, color: Colors.grey)),
-                      ],
-                    ),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
-                    Image.asset(
-                      'assets/images/sheffield.png',
-                    ),
-                  ],
-                ),
-                Padding(padding: EdgeInsets.all(5)),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25),
-                      child: Text(
-                        'Leicester',
+                                fontWeight: FontWeight.bold, fontSize: 30),
+                          ),
+                          Text('Fulltime',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 12, color: Colors.grey)),
+                        ],
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                      Image.asset(
+                        'assets/images/sheffield.png',
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Text(
+                          'Leicester',
+                          style: GoogleFonts.montserrat(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 55),
+                      ),
+                      Text(
+                        'Sheffield',
                         style: GoogleFonts.montserrat(
                             fontWeight: FontWeight.bold, fontSize: 16),
                       ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 5),
+          ),
+          TabBar(
+            controller: tabController,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.black,
+            tabs: [
+              Tab(
+                text: 'Timeline',
+              ),
+              Tab(
+                text: 'Lineups',
+              ),
+              Tab(
+                text: 'Stats',
+              ),
+              Tab(
+                text: 'News',
+              ),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: tabController,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Card(
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 40,
+                        width: 340,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.watch_later_outlined),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5),
+                            ),
+                            Text(
+                              'END OF MATCH',
+                              style: GoogleFonts.montserrat(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 55),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Card(
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 40,
+                        width: 340,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.watch_later_outlined),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5),
+                            ),
+                            Text(
+                              'END OF MATCH',
+                              style: GoogleFonts.montserrat(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    Text(
-                      'Sheffield',
-                      style: GoogleFonts.montserrat(
-                          fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Card(
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 40,
+                        width: 340,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.watch_later_outlined),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5),
+                            ),
+                            Text(
+                              'END OF MATCH',
+                              style: GoogleFonts.montserrat(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ],
-                )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Card(
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 40,
+                        width: 340,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.watch_later_outlined),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5),
+                            ),
+                            Text(
+                              'END OF MATCH',
+                              style: GoogleFonts.montserrat(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-        ),
+        ]),
       ),
     );
   }
